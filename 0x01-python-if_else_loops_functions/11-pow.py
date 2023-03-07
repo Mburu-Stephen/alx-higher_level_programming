75f818572c32fb2ecc8cb8a003ddfa2478eac22c
Mburu-Stephen <111083015+Mburu-Stephen@users.noreply.github.com>
2023-03-08 01:23:25 +0300

    Update 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python3
-remove_char_at = __import__('101-remove_char_at').remove_char_at
+def pow(a, b):
+    if b == 0:
+        return 1
+    elif b < 0:
+        return 1 / pow(a, -b)
+    elif b % 2 == 0:
+        return pow(a * a, b / 2)
+    else:
+        return a * pow(a, b - 1)
 
-print(remove_char_at("Holberton School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
