13b037340cb5de357c4bab27cb4ba12044b89d7c
Mburu-Stephen <111083015+Mburu-Stephen@users.noreply.github.com>
2023-04-12 13:15:46 +0300

    a function that writes a string to a text file (UTF8) and returns the number of characters written:

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Provides a function to count the lines in a file"""
+"""Provides a function to write text to a file"""
 
 
-def number_of_lines(filename=""):
-    """Count the number of lines in a file"""
-    with open(filename, 'r') as istream:
-        return len(istream.readlines())
+def write_file(filename="", text=""):
+    """Write text to a file"""
+    with open(filename, 'w') as ostream:
+        return ostream.write(text)
