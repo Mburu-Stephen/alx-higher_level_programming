dc04804c114b222af39262e7734c54a3bd813bdc
Mburu-Stephen <111083015+Mburu-Stephen@users.noreply.github.com>
2023-06-20 10:10:53 +0300

    a script that lists all states from the database hbtn_0e_0_usa:

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
-import MySQLdb
-import sys
+"""
+List all states from a MySQL db on localhost at port 3306
+"""
 
-if __name__ == "__main__":
+from mysqlman import MySQLMan
+from MySQLdb import Error
+from sys import argv, exit, stderr
 
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=sys.argv[1],
-                           passwd=sys.argv[2],
-                           db=sys.argv[3],
-                           charset="utf8")
-    # Start cursor
-    cur = conn.cursor()
 
-    # Query
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
+HELP = '{} username password database'.format(argv[0])
+HOST = 'localhost'
+PORT = 3306
 
-    # Print query
-    for row in query_rows:
+
+if __name__ == '__main__':
+    try:
+        mysqlman = MySQLMan(
+            connect=True,
+            user=argv[1],
+            password=argv[2],
+            database=argv[3],
+            host=HOST,
+            port=PORT,
+        )
+    except IndexError:
+        stderr.write('usage: {}\n'.format(HELP))
+        exit(2)
+    except Error as e:
+        stderr.write('{}\n'.format(e.args[1]))
+        exit(1)
+    query = "SELECT id, name FROM states ORDER BY id;"
+    results = mysqlman.query([query, ()])
+    for row in results[0]:
         print(row)
-
-    # Close cursor
-    cur.close()
-    conn.close()
